d0a144a604e68ec282978a8bc4d8990b9d57ed7f
postgres <djharga.com@gmail.com>
2025-07-05 20:23:33 +0200

    تحديث الإصدارات لتتوافق مع Netlify وإصدارات Node.js 14

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    target: 'es2020',
+    target: 'es2015',
     rollupOptions: {
       output: {
         manualChunks: {
